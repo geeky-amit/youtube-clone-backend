53be9db4b0faa5f77bf21282b336a4a14cb1ce65
geeky-amit <singh.amit_007@hotmail>
2023-12-06 15:52:50 +0530

    add user controllers and bug fixes

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    //upload file on the cloudinary server
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    //file has been successfully uploaded
+    console.log("file is uploaded successfully", response.url);
+    return response;
+  } catch (error) {
+    fs.unlink(localFilePath); // remove locally save temporary file as opration got failed
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
